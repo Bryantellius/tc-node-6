90cde698f666a5b3a1970a3060b0df8229bd0707
Ben <brbryant2639@gmail.com>
2022-02-11 11:46:26 -0600

    UPDATE: added node http walkthrough

--- walkthroughs/node/server.js
+++ walkthroughs/node/server.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const http = require("http"); // CommonJS syntax
+// import http from "http"    // ESM syntax
 
 class Math2 {
   static add(a, b) {
@@ -8,14 +9,51 @@ class Math2 {
 
 http
   .createServer((request, response) => {
-    let a = Math.round(Math.random() * 10);
-    let b = Math.round(Math.random() * 10);
+    let a = Math.round(Math.random() * 100);
+    let b = Math.round(Math.random() * 100);
 
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.write(
-      JSON.stringify({ problem: `${a} + ${b}`, answer: Math2.add(a, b) })
-    );
-    response.end();
+    let bufferChunks = [];
+
+    request.on("data", (chunk) => bufferChunks.push(chunk));
+
+    request.on("end", () => {
+      try {
+        if (request.url == "/") {
+          response.writeHead(200, { "Content-Type": "text/html" });
+          response.write("Home");
+        } else if (request.url == "/math") {
+          response.writeHead(200, { "Content-Type": "application/json" });
+          response.write(
+            JSON.stringify({ problem: `${a} + ${b}`, answer: Math2.add(a, b) })
+          );
+        } else if (request.url == "/test" && request.method == "POST") {
+          let body = JSON.parse(Buffer.concat(bufferChunks).toString());
+          body.confirmation = true;
+
+          response.writeHead(200, { "Content-Type": "application/json" });
+          response.write(JSON.stringify(body));
+        } else {
+          response.writeHead(404, { "Content-Type": "text/html" });
+          response.write("<h1>404 Not Found</h1><a href='/'>Try here.</a>");
+        }
+
+        response.end();
+      } catch (e) {
+        console.log(e);
+        response.writeHead(e.statusCode || 500, {
+          "Content-Type": "application/json",
+        });
+        response.write(
+          JSON.stringify({
+            error: {
+              ...e,
+            },
+            clientMessage: "An error occurred on the server. Try again later.",
+          })
+        );
+        response.end();
+      }
+    });
   })
   .listen(3000, () => {
     console.log("Server listening on port: " + 3000);
