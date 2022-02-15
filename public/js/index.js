d59cac527e5e2701f1ef813b07651fdb07f64c9e
Ben Bryant <brbryant2639@gmail.com>
2022-02-15 10:39:13 -0600

    UPDATE: time tests with remove duplicates function

--- public/js/index.js
+++ public/js/index.js
@@ -8,4 +8,17 @@ function pruneDups(list) {
   return [...new Set(list)];
 }
 
+function filterDups(list) {
+  return list.filter((num, index) => list.indexOf(num) == index);
+}
+
+// Avg time: 3.2ms
+console.time("Using Filter");
+console.log(filterDups(arr));
+console.timeEnd("Using Filter");
+
+// Avg time: 0.33ms
+// Using Set is 10x faster
+console.time("Using Set");
 console.log(pruneDups(arr));
+console.timeEnd("Using Set");
