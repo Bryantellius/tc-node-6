7b1862f6f0d6c7a57c7fd679c58630c45452fd4b
Ben Bryant <brbryant2639@gmail.com>
2022-02-16 12:09:43 -0600

    UPDATE: node streams walkthrough

--- walkthroughs/node/server.js
+++ walkthroughs/node/server.js
@@ -1,79 +1,112 @@
-const fs = require("fs");
 const http = require("http");
+const fs = require("fs");
 const path = require("path");
-
-const {
-  MIMETypes,
-  handleErrorResponse,
-  updateContactFile,
-} = require("./utils");
-const routes = require("./routes");
 const { EventEmitter } = require("events");
 
-const ContactEmitter = new EventEmitter();
+const port = 5001;
+const newsletter = new EventEmitter();
 
-ContactEmitter.on("new", (contact) =>
-  console.log(`${contact.name} just said: '${contact.message}'`)
-);
+// Create a server
+let server = http.createServer(requestHandler);
 
-const port = 5000;
+server.listen(port, () => console.log(`Server listening on port ${port}...`));
 
-http
-  .createServer((req, res) => {
-    let parsedPath = path.parse(req.url);
+// Define a request handler
+function requestHandler(req, res) {
+  // Handle the request body
+  const chunksArr = [];
 
-    req.on("error", (err) => handleErrorResponse(err, res));
-    res.on("error", (err) => handleErrorResponse(err, res));
-    // ContactEmitter.on("new", updateContactFile);
+  req.on("data", (chunk) => chunksArr.push(chunk));
 
+  req.on("end", () => {
+    // Handle a "/newsletter_signup" POST route
+    if (req.url == "/newsletter_signup" && req.method == "POST") {
+      let reqBody = Buffer.concat(chunksArr).toString();
 
-    if (parsedPath.ext) {
-      fs.readFile(
-        path.join(__dirname, "public", parsedPath.dir, parsedPath.base),
-        (err, contents) => {
-          if (err) {
-            return res.emit("error", err);
-          }
+      try {
+        reqBody = JSON.parse(reqBody);
+      } catch (e) {
+        console.error(e);
 
-          let contentType = MIMETypes[parsedPath.ext];
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.write(
+          JSON.stringify({
+            message: "You must send the request body in JSON format",
+          })
+        );
+        return res.end();
+      }
 
-          res.writeHead(200, {
-            "Content-Type": contentType,
-          });
-          res.write(contents);
-          res.end();
-        }
-      );
+      // emit a "signup" event
+      newsletter.emit("signup", { data: reqBody, res });
     } else {
-      let chunksArr = [];
-      let route = routes[req.method + req.url] || routes["*"];
+      let filePath = path.join(__dirname, `./public/notFound.html`);
+      let statusCode = 404;
 
-      ContactEmitter.on("new", (contact) => route.response(contact, res));
+      if (req.url == "/") {
+        filePath = path.join(__dirname, `./public/index.html`);
+        statusCode = 200;
+      } else if (req.url == "/about") {
+        filePath = path.join(__dirname, `./public/about.html`);
+        statusCode = 200;
+      }
 
-      req.on("data", (chunk) => chunksArr.push(chunk));
+      const readStream = fs.createReadStream(filePath);
 
-      req.on("end", () => {
-        if (req.method == "POST" || req.method == "PUT") {
-          let reqBody = JSON.parse(Buffer.concat(chunksArr).toString());
-          if (req.url == "/contact") {
-            ContactEmitter.emit("new", reqBody);
-          } else {
-            route.response(reqBody, res);
-          }
-        } else {
-          fs.readFile(route.filePath, (err, contents) => {
-            if (err) {
-              return res.emit("error", err);
-            }
-
-            res.writeHead(route.statusCode, {
-              "Content-Type": route.contentType,
-            });
-            res.write(contents);
-            res.end();
-          });
-        }
-      });
+      res.writeHead(statusCode, { "Content-Type": "text/html" });
+      readStream.pipe(res);
     }
-  })
-  .listen(port, () => console.log(`Server listening on port ${port}`));
+  });
+}
+
+newsletter.on("signup", updateNewsletter);
+
+function updateNewsletter({ data, res }) {
+  // append name + email record to a csv file
+  if (!data.name || !data.email) {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.write(
+      JSON.stringify({
+        message: `You must include values for both name and email.`,
+        status: "FAILED",
+        success: false,
+      })
+    );
+    return res.end();
+  }
+
+  let filePath = path.join(__dirname, "./data/newsletter.csv");
+  let contents = `${data.name},${data.email}\n`;
+
+  if (!fs.existsSync("./data")) fs.mkdirSync("data");
+
+  if (!fs.existsSync(filePath)) {
+    contents = `name,email\n` + contents;
+  }
+
+  fs.appendFile(filePath, contents, (err) => {
+    if (err) {
+      console.error(err);
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.write(
+        JSON.stringify({
+          message: "The server failed to update the newletter list",
+          status: "FAILED",
+          success: false,
+        })
+      );
+      res.end();
+    } else {
+      console.log(`Added ${data.name} to newsletter list!`);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(
+        JSON.stringify({
+          message: `Thank you, ${data.name}! You've been added to the newsletter list!`,
+          status: "SUCCEEDED",
+          success: true,
+        })
+      );
+      res.end();
+    }
+  });
+}
