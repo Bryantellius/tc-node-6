08c20426c193c9339ab6b3f86b2d730ac1a22cd4
Ben Bryant <brbryant2639@gmail.com>
2022-02-21 11:51:48 -0600

    UPDATE: added .env values

--- walkthroughs/expressandmysql/db/models/index.js
+++ walkthroughs/expressandmysql/db/models/index.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+let envFound = dotenv.config();
+
+if (!envFound) throw new Error("Cannot read .env file.");
 
 const mysqlConfig = {
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "bestbuy",
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 };
 
 const connection = mysql.createPool(mysqlConfig);
